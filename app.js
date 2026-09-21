9b1b4ce298d17e934797c2bf14ca7bf110d01fdd
BlackBerry KV <blackberry-kv@local>
2026-09-22 00:16:45 +0300

    Fix DATA_VER indentation

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
   const LEDGER_URL = "data/mvp-ledger.json";
   const TIERS_URL = "data/tiers.json";
   const FACTIONS_URL = "data/factions.json";
-const DATA_VER = "20260921-m2-fallujah";
+  const DATA_VER = "20260921-m2-fallujah";
   const ROSTER_URL = "https://bb-squad.ru/api/public/roster";
   const PROFILE_BASE = "https://bb-squad.ru/players";
   const FACTION_FALLBACK = {
