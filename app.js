15460854c2f176c09dfb6ce0c1cafa49123ce415
KeechBB <keech@users.noreply.github.com>
2026-09-23 22:45:48 +0300

    Add 23.09 Training Match 1 Mutaha AAS AFU 0:109 RGF.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@
   const TIERS_URL = "data/tiers.json";
   const FACTIONS_URL = "data/factions.json";
   const DATA_VER =
-    new URLSearchParams(location.search).get("v") || "20260922-fallujah-m3";
+    new URLSearchParams(location.search).get("v") || "20260923-mutaha-m1";
   const ROSTER_URL = "https://bb-squad.ru/api/public/roster";
   const PROFILE_BASE = "https://bb-squad.ru/players";
   const FACTION_FALLBACK = {
@@ -44,6 +44,7 @@
     IMF: "Сербы",
     ADF: "Австралия",
     PLA: "Китай",
+    AFU: "ВСУ",
   };
   const isEmbed =
     new URLSearchParams(location.search).has("embed") || window.self !== window.top;
