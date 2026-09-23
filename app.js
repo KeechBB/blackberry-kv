983fd148777b66252ce642974a7e58774709c81f
KeechBB <keech@users.noreply.github.com>
2026-09-24 00:25:48 +0300

    Add 23.09 Training Match 3 Chora OSI HOAK 94:0 AFU.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@
   const TIERS_URL = "data/tiers.json";
   const FACTIONS_URL = "data/factions.json";
   const DATA_VER =
-    new URLSearchParams(location.search).get("v") || "20260923-fallujah-m2b";
+    new URLSearchParams(location.search).get("v") || "20260923-chora-m3";
   const ROSTER_URL = "https://bb-squad.ru/api/public/roster";
   const PROFILE_BASE = "https://bb-squad.ru/players";
   const FACTION_FALLBACK = {
@@ -45,6 +45,7 @@
     ADF: "Австралия",
     PLA: "Китай",
     AFU: "ВСУ",
+    HOAK: "ХАОКИ",
   };
   const isEmbed =
     new URLSearchParams(location.search).has("embed") || window.self !== window.top;
